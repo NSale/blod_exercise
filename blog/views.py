7bcad76f9fd0106eccea74db812e0a842547879b
NSale <gospasrb@yahoo.com>
2021-09-20 23:42:39 +0200

    add templates setup

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('this is index function')
+    return render(request, "blog/index.html")
 
 
 def posts(request):
