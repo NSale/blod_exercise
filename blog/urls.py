7408d55ec3ca2abe491783b129114e8618aaf357
NSale <gospasrb@yahoo.com>
2021-09-22 00:14:42 +0200

    add dummy posts and page_detail page

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.index, name='index'),
     path('posts', views.posts, name='posts'),
     path('posts/<slug:slug>', views.individual_post, name='individual_post'),
-}
+]
