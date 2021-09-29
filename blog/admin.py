a8e1f7e06fcc214b76446dbd20426b3b060cad81
NSale <gospasrb@yahoo.com>
2021-09-29 23:14:38 +0200

    add Post, Author and Tag models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post, Author, Tag
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'date')
+    list_filter = ('title', 'date')
+    search_fields = ('title', 'date')
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    list_filter = ('first_name', 'last_name')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.caption
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    excerpt = models.TextField(default='', null=False, max_length=200)
+    image = models.CharField(max_length=50)
+    date = models.DateTimeField(verbose_name='date')
+    slug = models.SlugField(max_length=30, default='', null=False, db_index=True)
+    content = models.TextField(default='', null=False, max_length=2000)
+    author = models.ForeignKey(Author, null=False, on_delete=models.RESTRICT)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return f'{self.title} {self.date}'
