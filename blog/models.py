f92b994500f936d67990f565532318b756881f89
NSale <gospasrb@yahoo.com>
2021-10-17 22:07:05 +0200

    add post model

--- blog/models.py
+++ blog/models.py
@@ -31,3 +31,10 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.title} {self.date}'
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, null=True, on_delete=models.CASCADE, related_name='comments')
