61fc8c1e5544e4648de334cd4838fb1c0be951cc
NSale <gospasrb@yahoo.com>
2021-10-17 23:41:25 +0200

    add comment form validation styling

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.views import View
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from blog.models import Post
 from .forms import CommentForm
 
@@ -25,12 +29,30 @@ class Posts(ListView):
     context_object_name = 'posts'
 
 
-class IndividualPostView(DetailView):
-    template_name = "blog/post_detail.html"
-    model = Post
+class IndividualPostView(View):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tags'] = self.object.tags.all()
-        context['comment_form'] = CommentForm()
-        return context
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            'post': post,
+            'tags': post.tags.all(),
+            'comment_form': CommentForm(),
+        }
+        return render(request, 'blog/post_detail.html', context)
+
+    def post(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        comment_form = CommentForm(request.POST)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse('individual_post', args=[slug]))
+
+        context = {
+            'post': post,
+            'tags': post.tags.all(),
+            'comment_form': comment_form,
+        }
+        return render(request, 'blog/post_detail.html', context)
